cecef105a61fff8bc2c3e3f25f36d1afc1367088
Hasan Nur Towha <nurtowha@gmail.com>
2022-10-22 20:08:36 +0600

    csade

--- app/src/main/java/com/example/tuneinn/MainActivity.java
+++ app/src/main/java/com/example/tuneinn/MainActivity.java
@@ -9,7 +9,7 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {        //
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
         startActivity(new Intent(MainActivity.this, LoginActivity.class));
