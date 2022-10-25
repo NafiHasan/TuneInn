1ed44d987f0181c72c23671c5ded97c89e4f8aff
shakilmuhit <shakilmuhit2617@gmail.com>
2022-10-25 13:51:37 +0600

    playlist init

---
+++ app/src/main/java/com/example/tuneinn/Playlist.java
@@ -0,0 +1,32 @@
+package com.example.tuneinn;
+
+import java.util.ArrayList;
+
+public class Playlist {
+    String name;
+    ArrayList<Song> songs;
+
+    public Playlist(String name) {
+        this.name = name;
+    }
+
+    public void addSong(Song song){
+        songs.add(song);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(ArrayList<Song> songs) {
+        this.songs = songs;
+    }
+}
