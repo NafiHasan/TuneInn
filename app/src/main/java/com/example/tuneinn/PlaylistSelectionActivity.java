f56134e01735c9f66a0e171522b0ebafcbcc1fad
shakilmuhit <shakilmuhit2617@gmail.com>
2022-10-28 23:13:51 +0600

    functional playlist feature added

---
+++ app/src/main/java/com/example/tuneinn/PlaylistSelectionActivity.java
@@ -0,0 +1,50 @@
+package com.example.tuneinn;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class PlaylistSelectionActivity extends AppCompatActivity {
+    RecyclerView playlistsRecyclerView;
+    ArrayList<Playlist> playlists;
+    Button addButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlist);
+        setPlaylistsRecyclerView();
+        initViews();
+        handleEvents();
+    }
+
+    private void initViews() {
+        addButton= findViewById(R.id.add_new_playlist_button);
+    }
+
+    private void handleEvents() {
+       /* addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(PlaylistActivity.this,PlaylistCreateActivity.class);
+                startActivity((intent));
+            }
+        });*/
+    }
+
+    private void setPlaylistsRecyclerView() {
+        playlistsRecyclerView= findViewById(R.id.playlist_recycler_view);
+        //PlaylistInfo.allPlaylists= new ArrayList<>();
+        playlists= PlaylistInfo.allPlaylists;
+
+        playlistsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        playlistsRecyclerView.setAdapter(new PlaylistSelectionAdapter(playlists, getApplicationContext()));
+    }
+}
---
+++ app/src/main/java/com/example/tuneinn/PlaylistSelectionAdapter.java
@@ -0,0 +1,72 @@
+package com.example.tuneinn;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class PlaylistSelectionAdapter extends RecyclerView.Adapter<PlaylistSelectionAdapter.ViewHolder>{
+    ArrayList<Playlist> playlists;
+    Context context;
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView playlistName;
+        ViewHolder(View itemView){
+            super(itemView);
+            playlistName= itemView.findViewById(R.id.playlist_name);
+        }
+    }
+
+    public PlaylistSelectionAdapter(ArrayList<Playlist> playlists, Context context) {
+        this.playlists = playlists;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public PlaylistSelectionAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.playlist_recycler,parent,false);
+        return new PlaylistSelectionAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PlaylistSelectionAdapter.ViewHolder holder, int position) {
+        Playlist playlist= playlists.get(position);
+        ArrayList<Song> songs = playlist.getSongs();
+        holder.playlistName.setText(playlist.getName());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //PlaylistInfo.currentPlaylistPosition= holder.getAdapterPosition();
+                //RecyclerView songListRecyclerView = findViewById(R.id.songs_recycler_view);
+                /*MusicPlayer.getInstance().reset();
+                SongPosition.currentSongPosition = position;
+                Intent intent = new Intent(context, MusicPlayerActivity.class);
+                intent.putExtra("ABC", mySongs);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);*/
+                Song song= SongPosition.allSongs.get(SongPosition.selectedSongToAdd);
+                PlaylistInfo.allPlaylists.get(holder.getAdapterPosition()).addSong(song);
+                SongPosition.selectedSongToAdd= -1;
+                Toast.makeText(context, "Added to playlist "+playlist.getName(), Toast.LENGTH_SHORT).show();
+
+                Intent intent= new Intent(context,MusicActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return playlists.size();
+    }
+}
